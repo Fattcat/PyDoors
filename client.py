8be73f507bdbf6db71c17d30ba6c7c421d277b33
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-09-16 10:26:10 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 import socket
+import os
 
 # Nastavíme IP adresu a port servera
-server_ip = '192.168.0.174'  # Nahraďte skutočnou IP adresou servera
+server_ip = 'IP_adresa_servera'  # Nahraďte skutočnou IP adresou servera
 server_port = 8080  # Nahraďte skutočným portom servera
 
 # Pripojíme sa na server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server_ip, server_port))
+
+try:
+    s.connect((server_ip, server_port))
+    print("Pripojenie na server úspešné. Čakám na spustenie servera...")
+except ConnectionRefusedError:
+    print("Pripojenie na server neúspešné. Uistite sa, že server je spustený.")
+    os._exit(1)
 
 while True:
     # Získať príkaz od používateľa
@@ -18,9 +25,14 @@ while True:
     if command == "exit":
         break
 
+    if command == "clear":
+        os.system('clear')  # Linux príkaz na vymazanie obrazovky terminálu
+
     # Prijíma odpoveď od servera
     response = s.recv(1024).decode()
     print(response)
+
+print("Disconnected.")
 
 # Zatvoríme pripojenie
 s.close()
