8ff015b4e85a7d365900ebebf432305712275d60
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-09-16 11:31:30 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,13 @@
 import socket
 import os
 
-# Nastavíme IP adresu a port servera
-server_ip = '0.0.0.0'  # '0.0.0.0' znamená, že prijímame pripojenia na všetkých rozhraniach
-server_port = 8080  # Nahraďte skutočným portom servera
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
-# Spustíme server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((server_ip, server_port))
-s.listen(5)
-print(f"Server čaká na pripojenie na adrese {server_ip}:{server_port}")
-
-# Funkcia na získanie zoznamu súborov v aktuálnom adresári
 def list_files(directory):
     files = os.listdir(directory)
     return "\n".join(files)
 
-# Funkcia na zmenu adresára
 def change_directory(path):
     try:
         os.chdir(path)
@@ -24,41 +15,38 @@ def change_directory(path):
     except Exception as e:
         return f"Chyba pri zmene adresára: {str(e)}"
 
-# Priekžem pripojenie od klienta
-client_socket, addr = s.accept()
-print(f"Pripojenie od {addr} úspešné. Čakám na príkazy od klienta...")
+def send_file(client_socket, file_name):
+    try:
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+        client_socket.send(file_data)
+    except FileNotFoundError:
+        client_socket.send(b"FileNotFound")
+
+server_ip = '192.168.0.174'
+server_port = 8080 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.connect((server_ip, server_port))
+    print("Pripojenie na server úspešné. Čakám na spustenie servera...")
+except ConnectionRefusedError:
+    print("Pripojenie na server neúspešné. Uistite sa, že server je spustený.")
+    os._exit(1)
 
 while True:
-    # Prijímame príkaz od klienta
-    command = client_socket.recv(1024).decode()
-
-    if not command:
-        break
+    command = input("Zadajte príkaz: ")
+    s.send(command.encode())
 
     if command == "exit":
         break
-    elif command == "show_files":
-        files = list_files(os.getcwd())
-        client_socket.send(files.encode())
-    elif command.startswith("change_dir "):
-        new_dir = command.split(" ")[1]
-        response = change_directory(new_dir)
-        client_socket.send(response.encode())
-    elif command.startswith("download "):
-        file_name = command.split(" ")[1]
-
-        try:
-            with open(file_name, "rb") as file:
-                file_data = file.read()
-            client_socket.send(file_data)
-        except FileNotFoundError:
-            client_socket.send(b"FileNotFound")
 
     if command == "clear":
-        os.system('clear')  # Linux príkaz na vymazanie obrazovky terminálu
+        clear_screen()
+
+    response = s.recv(1024).decode()
+    print(response)
 
 print("Disconnected.")
-
-# Zatvoríme pripojenie
-client_socket.close()
 s.close()
