d4940570ab9aeb00f0860dfc855068ac5d810e15
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-16 16:37:43 +0100

    Create TestClient.py

---
+++ TestClient.py
@@ -0,0 +1,46 @@
+import socket
+import sys
+import threading
+
+def receive_data(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
+            break
+        print(data)
+
+def send_data(client_socket):
+    while True:
+        message = input()
+        client_socket.send(message.encode())
+        if message.lower() == "exit":
+            break
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Zadejte adresu a port servera
+    server_address = ('IPAdress', 12345)
+
+    try:
+        client_socket.connect(server_address)
+        print("Connected to the server.")
+
+        receive_thread = threading.Thread(target=receive_data, args=(client_socket,))
+        send_thread = threading.Thread(target=send_data, args=(client_socket,))
+
+        receive_thread.start()
+        send_thread.start()
+
+        receive_thread.join()
+        send_thread.join()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        client_socket.close()
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
